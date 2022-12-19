939db93bc16114012f62162e76800a21eee5046c
kaiyumPC <kaiyumcg@gmail.com>
2022-12-19 07:19:23 +0600

    Update Util.cs

--- Editor/Util.cs
+++ Editor/Util.cs
@@ -7,7 +7,7 @@ using UnityEditorInternal;
 
 public static class Util
 {
-    [MenuItem(itemName: "Assets/Create/UITween/Script/Create New UI Tween Script", isValidateFunction: false, priority: 51)]
+    [MenuItem(itemName: "Assets/Create/Kaiyum/UITween/Script/Create New UI Tween Script", isValidateFunction: false, priority: 51)]
     public static void CreateScriptFromTemplate()
     {
         KEditorUtil.ProjectResourceUtil.CreateScriptFromTemplate("NewUITween", "UITween", "Editor", "tmpl");
